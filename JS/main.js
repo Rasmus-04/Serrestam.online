4a5b67bb64fbaf65c000be21fc8ea1881add1489
Rasmus Serrestam <r.serrestam@gmail.com>
2021-11-14 11:57:56 +0100

    Added game

--- JS/main.js
+++ JS/main.js
@@ -3,8 +3,9 @@ function toggle_navbar() {
     var x = document.getElementById("responsive_navbar");
     if (x.className === "Header-bar") {
       x.className += " responsive";
+      document.getElementById("unity-container").style.marginTop = "225px";
     } else {
       x.className = "Header-bar";
+      document.getElementById("unity-container").style.marginTop = "50px";
     }
   }
-  
